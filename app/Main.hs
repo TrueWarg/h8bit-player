7b4a154d2997ada3a14599824aedb0fe20fe45ec
TrueWarg <irrmm9000@gmail.com>
2022-05-24 23:57:15 +0200

    Add monomer sample

--- app/Main.hs
+++ app/Main.hs
@@ -1,6 +1,61 @@
+{-# LANGUAGE OverloadedStrings #-}
+{-# LANGUAGE TemplateHaskell #-}
+
 module Main where
 
-import Player
+import Control.Lens
+import Data.Text (Text)
+import Monomer
+import TextShow
+
+import qualified Monomer.Lens as L
+
+newtype AppModel = AppModel {
+  _clickCount :: Int
+} deriving (Eq, Show)
+
+data AppEvent
+  = AppInit
+  | AppIncrease
+  deriving (Eq, Show)
+
+makeLenses 'AppModel
+
+buildUI
+  :: WidgetEnv AppModel AppEvent
+  -> AppModel
+  -> WidgetNode AppModel AppEvent
+buildUI wenv model = widgetTree where
+  widgetTree = vstack [
+      label "Chebureck",
+      spacer,
+      hstack [
+        label $ "Click count: " <> showt (model ^. clickCount),
+        spacer,
+        button "Increase count" AppIncrease
+      ]
+    ] `styleBasic` [padding 10]
+
+handleEvent
+  :: WidgetEnv AppModel AppEvent
+  -> WidgetNode AppModel AppEvent
+  -> AppModel
+  -> AppEvent
+  -> [AppEventResponse AppModel AppEvent]
+handleEvent wenv node model evt = case evt of
+  AppInit -> []
+  AppIncrease -> [Model (model & clickCount +~ 1)]
 
 main :: IO ()
-main = someFunc
+main = do
+  startApp model handleEvent buildUI config
+  where
+    config = [
+      appWindowTitle "Lol kek",
+      appWindowIcon "./assets/images/icon.bmp",
+      appTheme darkTheme,
+      appFontDef "Regular" "./assets/fonts/Roboto-Regular.ttf",
+      appInitEvent AppInit
+      ]
+    model = AppModel 0
+
